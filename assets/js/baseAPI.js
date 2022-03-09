ffd814983294c3ad4fe8b1cd324fc0e72f249513
andy <584755025@qq.com>
2022-03-09 16:52:47 +0800

    完成个人中心功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,15 +1,15 @@
-$.ajaxPrefilter(function(options){
-    options.url='http://www.liulongbin.top:3007'+options.url;
-    if(options.url.indexOf('/my') != -1){
-        options.headers={
-            Authorization:localStorage.getItem('token')||''
+$.ajaxPrefilter(function (options) {
+    options.url = 'http://www.liulongbin.top:3007' + options.url;
+    if (options.url.indexOf('/my') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
         }
     }
-    options.complete=function(res){
-        if(res.responseJSON.status ===1 && res.responseJSON.message==='身份认证失败！'){
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             localStorage.removeItem('token');
-            location.href='http://127.0.0.1:5500/04/03/code/login.html';
+            location.href = 'http://127.0.0.1:5500/04/03/code/login.html';
         }
     }
-    console.log(options);
+    // console.log(options);
 })
